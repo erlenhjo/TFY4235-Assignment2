b3cae18516c49e5f6b9f817d0d341fe1da0111e7
erlenhjo <erlendhjohansen@gmail.com>
2022-04-16 22:23:48 +0200

    streamline data gen

--- dataGeneration.py
+++ dataGeneration.py
@@ -40,80 +40,42 @@ import units
 
 
 
-#Simple function for creating a folder and all missing previous folders
-#If the folder already exists, the error is printed rather than raised
+#Function for creating a folder and all missing previous folders
+#If the for instance the folder already exists, the error is printed rather than raised
 def create_folder(path):
     try:
         os.makedirs(path)
     except OSError as err: 
         print(err)
-
-
-def generate_particle_tracks_flashing(radius_name, tau, N, particle_count):
-    global r_1, eV, L, alpha, eta, kbT
+        
+def folder_path_constant(radius_name,deltaU,N):
+    deltaU_per_kbT=deltaU/kbT
+    folder_path=os.path.join("raw_data","constant_potential",radius_name,f"N_{N}",f"deltaU_{deltaU_per_kbT:.2f}kbT")
+    return folder_path
     
-    #setting and calculating experiment variables
-    deltaU=80*eV
-    if radius_name=="r_1":
-        r=r_1
-    elif radius_name=="r_2":
-        r=3*r_1
-    gamma=units.gamma(eta, r)
-    omega=units.omega(deltaU, gamma, L)
-    D_hat=units.D_hat(kbT, deltaU)    
-    dt=units.dt(gamma, kbT, alpha, L, deltaU)
-    dt_hat=units.dt_hat(dt, omega)
-    flashing=True   #flashing potential
-    
-    #aquiring a suitable seed for the PRNG from computer entropy
-    rng_seed=np.random.SeedSequence().entropy  
-    
-    #calculating the x positions of the particles through the euler scheme
-    x_hat_vals=euler.execute_euler_scheme(particle_count,dt_hat,tau,alpha,omega,D_hat,N,rng_seed,flashing)
-
-    #create dict for metadata and add values
-    metadata={}
-    metadata["deltaU"]=deltaU
-    metadata["r"]=r
-    metadata["gamma"]=gamma
-    metadata["omega"]=omega
-    metadata["D_hat"]=D_hat
-    metadata["dt"]=dt
-    metadata["dt_hat"]=dt_hat
-    metadata["particle_count"]=particle_count
-    
-    #create the path for the data folder
-    folder_path=os.path.join("raw_data","flashing_potential",radius_name,f"N_{N}",f"tau_{tau}",str(rng_seed))
-    #create said folder and all missing intermediate folders
-    create_folder(folder_path)
-    
-    #save x values and metadata to file
-    x_path=os.path.join(folder_path, "x_hat_vals.npy")
-    metadata_path=os.path.join(folder_path, "metadata.npy")
-    np.save(x_path,x_hat_vals)
-    np.save(metadata_path,metadata)
-    
-def generate_particle_tracks_constant(radius_name, deltaU_per_kbT, N, particle_count):
+def folder_path_flashing(radius_name,tau,N):
+    folder_path=os.path.join("raw_data","flashing_potential",radius_name,f"N_{N}",f"tau_{tau}")
+    return folder_path
+        
+def generate_particle_tracks(radius_name, N, particle_count, deltaU, tau, flashing):
     global r_1, L, alpha, eta, kbT
     
-    tau=1   #irrelevant as there is no flashing, but necessary as a function input
-    deltaU=deltaU_per_kbT*kbT
     if radius_name=="r_1":
         r=r_1
     elif radius_name=="r_2":
         r=3*r_1
+    
     gamma=units.gamma(eta, r)
     omega=units.omega(deltaU, gamma, L)
     D_hat=units.D_hat(kbT, deltaU)    
     dt=units.dt(gamma, kbT, alpha, L, deltaU)
     dt_hat=units.dt_hat(dt, omega)
-    flashing=False  #constant potential
     
     #aquiring a suitable seed for the PRNG from computer entropy
     rng_seed=np.random.SeedSequence().entropy  
 
     #calculating the x positions of the particles through the euler scheme    
-    x_hat_vals=euler.execute_euler_scheme(particle_count,dt_hat,tau,alpha,omega,D_hat,N,rng_seed,flashing)
+    trajectories=euler.execute_euler_scheme(particle_count,dt_hat,tau,alpha,omega,D_hat,N,rng_seed,flashing)
 
     #create dict for metadata and add values
     metadata={}
@@ -125,18 +87,48 @@ def generate_particle_tracks_constant(radius_name, deltaU_per_kbT, N, particle_c
     metadata["dt"]=dt
     metadata["dt_hat"]=dt_hat
     metadata["particle_count"]=particle_count
+    metadata["flashing"]=flashing
+    metadata["rng_seed"]=rng_seed
+    metadata["N"]=N
+    metadata["tau"]=tau
     
-    #create the path for the data folder
-    folder_path=os.path.join("raw_data","constant_potential",radius_name,f"N_{N}",f"deltaU_{deltaU_per_kbT}kbT",str(rng_seed))
-    #create said folder and all missing intermediate folders
-    create_folder(folder_path)
+    #set the path for the outer data folder
+    if flashing:
+        folder_path=folder_path_flashing(radius_name, tau, N)
+    else:
+        folder_path=folder_path_constant(radius_name, deltaU, N)
+    
+    #create the seed folder and all missing intermediate folders
+    create_folder(os.path.join(folder_path,str(rng_seed)))
     
     #save x values and metadata to file
-    x_path=os.path.join(folder_path, "x_hat_vals.npy")
-    metadata_path=os.path.join(folder_path, "metadata.npy")
-    np.save(x_path,x_hat_vals)
-    np.save(metadata_path,metadata)
+    trajectories_path=os.path.join(folder_path, str(rng_seed), "trajectories.npy")
+    metadata_path=os.path.join(folder_path, str(rng_seed), "metadata.npy")
+    np.save(trajectories_path, trajectories)
+    np.save(metadata_path, metadata)
+
+
+def get_available_seeds(radius_name, N, deltaU, tau, flashing):
+    #set outer folder path
+    if flashing:
+        folder_path=folder_path_flashing(radius_name, tau, N)
+    else:
+        folder_path=folder_path_constant(radius_name, deltaU, N)
     
-if __name__=="__main__":
-    generate_particle_tracks_flashing("r_1", 1, 1000, 10)
-    generate_particle_tracks_constant("r_1", 1, 1000, 10)
+    #get seeds from folder names
+    seeds=os.listdir(folder_path)
+    
+    #find particle count for all seeds
+    particle_counts=[]
+    for seed in seeds:
+        #set path to metadata file
+        metadata_path=os.path.join(folder_path,seed,"metadata.npy")
+        #load metadata dict from file
+        metadata=np.load(metadata_path, allow_pickle=True).item()
+        #extract particle count
+        particle_counts.append(metadata["particle_count"])
+        
+    return seeds, particle_counts
+
+    
+    
